d8c051a34ae5de63316eed6f7573d8dccb8ce744
Ibukun <ibukunagbo@gmail.com>
2022-10-13 15:16:00 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import FormCard  from "./component/FormCard.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React CheckPoints</h1>
+      <FormCard /> 
+    </div>
+  )
+}
+
+export default App
